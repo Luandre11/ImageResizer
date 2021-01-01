c881e3c54ee06bab7fab94b1bb565cac644446ae
Gabs12 <lucianolomelin@gmail.com>
2021-01-01 16:18:45 -0600

    started the actual code

--- src/functions.py
+++ src/functions.py
@@ -3,3 +3,49 @@ from PIL import Image
 import os 
 import json
 
+
+class Image_Resize():
+    def __init__(self):
+        self.example = {"image": "test.png", "size_x": 100, "size_y": 100}
+        self.path = "src\\"
+
+        if os.path.isfile(f"src\\information.json"):
+            with open("src\\information.json", "r") as f:
+                self.example = json.load(f)
+    
+    def save(self):
+       with open("src\\information.json", "w") as f:
+           json.dump(self.example, f)
+    
+    
+    def create_folder1(self): 
+        if os.path.isdir("images") == False:
+            try:
+               os.mkdir("images")   
+            except:
+                print("something went wrong while making the directory")
+
+            
+    def get_image(self):
+        with open("src\\information.json", "r") as f:
+            data = json.load(f)
+            image = data["image"]
+            if image in os.listdir("images"):
+                if image.endswith(".png")  or image.endswith (".jpg"):
+                    return image
+            
+            
+test = Image_Resize()
+
+test.create_folder1()
+test.save()
+print(test.get_image())
+        
+        
+                
+  
+
+
+                    
+    
+     
